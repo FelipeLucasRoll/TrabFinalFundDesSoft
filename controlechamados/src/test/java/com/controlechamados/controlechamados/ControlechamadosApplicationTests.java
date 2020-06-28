1193188b52e5ef56c5fcaa642e123cbbc27e643e
Douglas Pacheco <douglas.m.pacheco@outlook.com>
2020-06-28 15:35:01 -0300

    relocating files

---
+++ controlechamados/src/test/java/com/controlechamados/controlechamados/ControlechamadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controlechamados.controlechamados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlechamadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
