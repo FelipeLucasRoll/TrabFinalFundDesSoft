49811fa073d7f1bd3333f23a93c50cbf51328668
FelipeLucasRoll <flp_roll@hotmail.com>
2020-06-29 14:41:30 -0300

    dados sql

---
+++ controlechamados/src/main/resources/db/data.sql
@@ -0,0 +1,64 @@
+INSERT INTO Chamado (idchamado,idcliente,idtecnico,status1,descricao,horaAbertura,horaFechamento,prazoLimite,prioridade) VALUES 
+(12345,67890,02873,"Cancelado","Cliente cancelou o servico",'Mon Mar 14 11:48:49 BRT 2011','Tue Mar 15 13:05:35 BRT 2011','Sun Mar 20 05:00:00 BRT 2011',1);
+
+
+INSERT INTO Chamado (idchamado,idcliente,idtecnico,status1,descricao,horaAbertura,horaFechamento,prazoLimite,prioridade) VALUES 
+(54321,09876,35643,"Aprovado","Computador nao liga",'Wed Mar 10 14:56:21 BRT 2020','Fri Mar 12 15:00:00 BRT 2020','Wed Mar 17 16:00:00 BRT 2020',2);
+
+INSERT INTO Chamado (idchamado,idcliente,idtecnico,status1,descricao,horaAbertura,horaFechamento,prazoLimite,prioridade) VALUES 
+(12543,60798,02873,"Arrumado","Computador desliga sozinho",'Sun Jan 01 10:12:34 BRT 2019','Thu Jan 05 13:05:00 BRT 2019','Sun Jan 15 16:00:00 BRT 2019',1);
+
+INSERT INTO Chamado (idchamado,idcliente,idtecnico,status1,descricao,horaAbertura,horaFechamento,prazoLimite,prioridade) VALUES 
+(23514,68978,35643,"Arrumado","Computador nao liga",'Sat Oct 25 09:44:50 BRT 2018','Sun Oct 26 14:01:31 BRT 2018','Fri Oct 30 22:00:00 BRT 2018',2);
+
+INSERT INTO Chamado (idchamado,idcliente,idtecnico,status1,descricao,horaAbertura,horaFechamento,prazoLimite,prioridade) VALUES 
+(13524,67987,78554,"Aprovado","Limpeza",'Tue Dec 16 13:38:51 BRT 2020','Thu Dec 18 13:56:45 BRT 2020','Sat Dec 20 19:00:00 BRT 2020',1);
+
+
+
+INSERT INTO Cliente (idcliente,nome,cpf,datanasc,telefone,email,login1,senha,plano) VALUES
+(67890,"Sebastiana Luana","358.772.576-41","26/08/1985","99107-8013","ssebastianaluana@nextel.com.br","ssbast","43EKc2yzKj","Basic");
+
+INSERT INTO Cliente (idcliente,nome,cpf,datanasc,telefone,email,login1,senha,plano) VALUES
+(09876,"Caio Márcio Figueiredo","636.086.287-54","10/08/1954","99209-8108","caiomarciofigueiredo_@hushmail.com","caiomf","MsM4uDgA4R","Basic");
+
+INSERT INTO Cliente (idcliente,nome,cpf,datanasc,telefone,email,login1,senha,plano) VALUES
+(60798,"Caroline Raimunda Pereira","Caroline Raimunda Pereira","18/03/1998","98689-6288","carolineraimundapereira@mpeventos.com.br","crp10","qyPUcwjn7o","Premium");
+
+INSERT INTO Cliente (idcliente,nome,cpf,datanasc,telefone,email,login1,senha,plano) VALUES
+(68978,"Fátima Marcela Fogaça","810.018.258-29","10/08/1976","99922-9896","fatimamarcelafogaca72@maissaude.adm.br","ff72","70x8582Ljp","Basic");
+
+INSERT INTO Cliente (idcliente,nome,cpf,datanasc,telefone,email,login1,senha,plano) VALUES
+(67987,"Ryan Roberto Dias","572.458.323-23","17/10/1977","99434-2597","ryanrobertodias@cartovale.com.br","rrdias","cSAxCn17Dc","Premium");
+
+
+
+
+INSERT INTO Habilidade (idhabilidade,nome,tipoHS,manager) VALUES
+(10,"Manutencao",'H',false);
+
+INSERT INTO Habilidade (idhabilidade,nome,tipoHS,manager) VALUES
+(11,"Formatacao",'S',false);
+
+INSERT INTO Habilidade (idhabilidade,nome,tipoHS,manager) VALUES
+(12,"Instalacao de Softwares",'S',true);
+
+
+
+INSERT INTO Tecnico (idtecnico,nome,cpf,login1,senha,habilidades,avaliacao) VALUES
+(35643,"Kaique Emanuel","360.394.593-01","ke1010","85GCOGguD2","Manutencao",9.8);
+
+INSERT INTO Tecnico (idtecnico,nome,cpf,login1,senha,habilidades,avaliacao) VALUES
+(02873,"Roberto Kaique","278.148.397-42","rkaique2","zPaqXzfrtO","Formatacao",7.0);
+
+INSERT INTO Tecnico (idtecnico,nome,cpf,login1,senha,habilidades,avaliacao) VALUES
+(78554,"Isis Isabel Gomes","533.030.122-02","isiIg","Pn3lrRm0f1","Manutencao",10);
+
+INSERT INTO Tecnico (idtecnico,nome,cpf,login1,senha,habilidades,avaliacao) VALUES
+(78554,"Pedro Henrique","721.705.436-27","ph01","4Xrhh46626","Instalacao de Softwares",9.5);
+
+
+
+
+
+
