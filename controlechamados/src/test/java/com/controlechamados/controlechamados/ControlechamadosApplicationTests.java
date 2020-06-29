60fe13dbee1e0d90b95cf56edd0c41d0b3c3e4ab
Douglas Pacheco <douglas.m.pacheco@outlook.com>
2020-06-28 23:40:11 -0300

    Insert SpringBoot annotations

--- controlechamados/src/test/java/com/controlechamados/controlechamados/ControlechamadosApplicationTests.java
+++ controlechamados/src/test/java/com/controlechamados/controlechamados/ControlechamadosApplicationTests.java
@@ -1,5 +1,7 @@
 package com.controlechamados.controlechamados;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.controlechamados.Entity.Chamado;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +11,20 @@ class ControlechamadosApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@BeforeEach
+	public void setup(){
+    Chamado c = new Chamado("ok");
+	}
+
+
+	@Test
+	public void testaStatus(){
+    String status = c.getStatus();
+    String s = "ok";
+    assertEquals(s, status);
+	}
+
+
 
 }
