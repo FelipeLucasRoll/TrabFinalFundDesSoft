a34ba716708d4832bd10d9684e1da8ee73b6376b
Douglas Pacheco <douglas.m.pacheco@outlook.com>
2020-07-02 14:30:31 -0300

    receive form from frontend post

--- controlechamados/src/main/java/com/controlechamados/Interface/Webserver/ClienteFachadaRemota.java
+++ controlechamados/src/main/java/com/controlechamados/Interface/Webserver/ClienteFachadaRemota.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/servcliente")
+@RequestMapping(path = "/servcliente",consumes = "*")
 public class ClienteFachadaRemota {
 
 	private ServicosCliente servCliente;
